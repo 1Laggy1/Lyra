20fe2e8ea4d6fefca399efd546ec5e915ed7aa02
1_laggy_1 <andryushka05@gmail.com>
2024-09-12 13:44:54 +0300

    Starter commit and some start code
    - added unity project
    - added few scripts: PlayerMovement, CharacterController2D from Brackeys to speed up development, also changed jump mechanic to be more suitable.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController2D CC;
+    float horizontalMove;
+    [SerializeField]
+    float runSpeed;
+    bool crouch;
+    bool jump;
+
+    public float maxJumpInput = 1f;
+    public float jumpInputSpeed = 2f;
+    private float currentJumpInput = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CC = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal")*runSpeed;
+        if (Input.GetButton("Jump"))
+        {
+            // Gradually increase the jump input value up to maxJumpInput
+            currentJumpInput = Mathf.Lerp(currentJumpInput, maxJumpInput, jumpInputSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // Gradually decrease the jump input value down to 0
+            currentJumpInput = Mathf.Lerp(currentJumpInput, 0f, jumpInputSpeed * Time.deltaTime);
+        }
+
+        // Debug or use the jump input value to control jumping force
+        Debug.Log("Current Jump Input: " + currentJumpInput);
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } 
+        else
+        {
+            crouch = false;
+        }
+    }
+    void FixedUpdate ()
+    {
+        Debug.Log(Input.GetAxisRaw("Jump"));
+        CC.Move(horizontalMove*Time.fixedDeltaTime, crouch, jump, currentJumpInput );
+        jump = false;
+    }
+}
